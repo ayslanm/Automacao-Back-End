df80e5109229e13006a9a6aa10a8bfa6efb116c9
ayslankvm <ayslankvm@gmail.com>
2024-06-06 16:38:37 -0300

    Adicionado Put step

--- features/step_definitions/employee.rb
+++ features/step_definitions/employee.rb
@@ -34,3 +34,30 @@ Entao('esse funcionario sera cadastrado') do
     expect(@create_employee["employee_salary"]).to eql (420800)
     expect(@create_employee["employee_age"]).to eql (30)
 end
+
+Dado('que o usuario altere uma informacao de funcionario') do
+    @get_employee = HTTParty.get('http://dummy.restapiexample.com/api/v1/employees', :headers => {'Content-Type': 'application/json'})
+    puts @get_employee['data'][0]['id']
+    @put_url = 'https://dummy.restapiexample.com/api/v1/update/' + @get_employee['data'][0]['id'].to_s
+end
+  
+Quando('ele enviar as novas informacoes') do
+    @update_employee = HTTParty.put(@put_url, :headers => {'Content-Type': 'application/json'}, body: {
+        "employee_name": "Alberto",
+        "employee_salary": 100,
+        "employee_age": 40,
+        "profile_image": ""
+}.to_json)
+
+    puts(update_employee)
+end
+  
+Entao('as informacoes serao alteradas') do
+    expect(@update_employee.code).to eql (200)
+    expect(update_employee.msg).to eql 'OK'
+    expect(update_employee["status"]).to eql 'success'
+    expect(update_employee["status"]). to eql 'Successfully! Record has been updated'
+    expect(update_employee['data']["employee_name"]).to eql 'Alberto'
+    expect(update_employee['data']["employee_salary"]).to eql (100)
+    expect(update_employee['data']["employee_age"]).to eql (40)
+end
